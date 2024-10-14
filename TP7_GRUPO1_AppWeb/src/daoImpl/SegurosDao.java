4fedec7e63dfb0056dea5dfd15d84d73bed5a6ad
maximo0001 <maximo148601@gmail.com>
2024-10-13 21:06:22 -0300

    add: métodos agregar y obtener seguros

--- TP7_GRUPO1_AppWeb/src/daoImpl/SegurosDao.java
+++ TP7_GRUPO1_AppWeb/src/daoImpl/SegurosDao.java
@@ -14,6 +14,127 @@ import entidad.Seguro;
 public class SegurosDao {
 	
 	
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "segurosGroup";
 
+	
+	public int agregarSeguro(Seguro seguro) {
+	    int filas = 0;
+	    Connection cn = null;
+	    try {
+	        // Cargar el driver de MySQL
+	        Class.forName("com.mysql.jdbc.Driver");
+
+	        // Conectarse a la base de datos
+	        cn = DriverManager.getConnection(host + dbName, user, pass);
+
+	        // Preparar la consulta con PreparedStatement para evitar inyección SQL
+	        String query = "INSERT INTO seguros (descripcion, idTipo, costoContratacion, costoAsegurado) VALUES (?, ?, ?, ?)";
+	        PreparedStatement pst = cn.prepareStatement(query);
+	        
+	        // Asignar valores a los parámetros
+	        pst.setString(1, seguro.getDescripcion());
+	        pst.setInt(2, seguro.getIdTipo());
+	        pst.setBigDecimal(3, BigDecimal.valueOf(seguro.getCostoContratacion()));
+	        pst.setBigDecimal(4, BigDecimal.valueOf(seguro.getCostoAsegurado()));
+
+	        // Ejecutar la actualización
+	        filas = pst.executeUpdate();
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        // Cerrar la conexión para liberar recursos
+	        if (cn != null) {
+	            try {
+	                cn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	    return filas;
+	}
+
+	
+	
+	
+	public ArrayList<Seguro> obtenerSeguros() {
+	    ArrayList<Seguro> lista = new ArrayList<>();
+	    Connection conn = null;
+	    PreparedStatement pstmt = null;
+	    ResultSet rs = null;
+
+	    try {
+	        conn = DriverManager.getConnection(host + dbName, user, pass);
+	        String query = "SELECT idSeguro, descripcion, idTipo, costoContratacion, costoAsegurado FROM seguros";
+	        pstmt = conn.prepareStatement(query);
+	        rs = pstmt.executeQuery();
+
+	        while (rs.next()) {
+	            Seguro seguro = new Seguro();
+	            seguro.setIdSeguro(rs.getInt("idSeguro"));
+	            seguro.setDescripcion(rs.getString("descripcion"));
+	            seguro.setIdTipo(rs.getInt("idTipo"));
+	            seguro.setCostoContratacion(rs.getDouble("costoContratacion"));
+	            seguro.setCostoAsegurado(rs.getDouble("costoAsegurado"));
+
+	            lista.add(seguro);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (rs != null) rs.close();
+	            if (pstmt != null) pstmt.close();
+	            if (conn != null) conn.close();
+	        } catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	    }
+
+	    return lista;
+	}
+
+	
+	
+	public Seguro obtenerUnSeguro(int id) {
+	    Seguro seguro = null;
+	    Connection con = null;
+	    PreparedStatement pstmt = null;
+	    ResultSet resultado = null;
+
+	    try {
+	        con = DriverManager.getConnection(host + dbName, user, pass);
+	        String query = "SELECT idSeguro, descripcion, idTipo, costoContratacion, costoAsegurado FROM seguros WHERE idSeguro = ?";
+	        pstmt = con.prepareStatement(query);
+	        pstmt.setInt(1, id); // Cargo el ID recibido
+	        resultado = pstmt.executeQuery();
+
+	        if (resultado.next()) {
+	            seguro = new Seguro();
+	            seguro.setIdSeguro(resultado.getInt("idSeguro"));
+	            seguro.setDescripcion(resultado.getString("descripcion"));
+	            seguro.setIdTipo(resultado.getInt("idTipo"));
+	            seguro.setCostoContratacion(resultado.getDouble("costoContratacion"));
+	            seguro.setCostoAsegurado(resultado.getDouble("costoAsegurado"));
+	        }
+	    } catch (Exception e) {
+	        System.out.println("Error de conexión o consulta: " + e.getMessage());
+	    } finally {
+	        try {
+	            if (resultado != null) resultado.close();
+	            if (pstmt != null) pstmt.close();
+	            if (con != null) con.close();
+	        } catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	    }
+	    
+	    return seguro;
+	}	
 
 }
--- TP7_GRUPO1_AppWeb/src/main/Principal.java
+++ TP7_GRUPO1_AppWeb/src/main/Principal.java
@@ -1,13 +1,66 @@
 package main;
 
+import java.util.ArrayList;
 
+import daoImpl.SegurosDao;
+import entidad.Seguro;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SegurosDao seguroDAO = new SegurosDao();
+
+        // Probar método para insertar un nuevo seguro
+        Seguro nuevoSeguro = new Seguro();
+        nuevoSeguro.setDescripcion("Seguro de Vida2");
+        nuevoSeguro.setIdTipo(1); // Por ejemplo, 1 podría representar "Vida"
+        nuevoSeguro.setCostoContratacion(500.75);
+        nuevoSeguro.setCostoAsegurado(300.50);
+        
+        // Llamar al método agregarSeguro para insertar el nuevo seguro
+        int resultado = seguroDAO.agregarSeguro(nuevoSeguro);
+
+        // Verificar si la inserción fue exitosa
+        if (resultado > 0) {
+            System.out.println("Seguro insertado correctamente.");
+        } else {
+            System.out.println("No se pudo insertar el seguro.");
+        }
+        
+        
+        
+	
+        //seguroDAO.procedimientoInsertarSeguro(nuevoSeguro);
+        //System.out.println("Seguro insertado correctamente.");
+        
+        
+      
+        ArrayList<Seguro> listaSeguros = seguroDAO.obtenerSeguros();
+        for (Seguro seguro : listaSeguros) {
+            System.out.println("ID: " + seguro.getIdSeguro());
+            System.out.println("Descripción: " + seguro.getDescripcion());
+            System.out.println("Tipo de Seguro: " + seguro.getIdTipo());
+            System.out.println("Costo de Contratación: " + seguro.getCostoContratacion());
+            System.out.println("Costo Asegurado: " + seguro.getCostoAsegurado());
+            System.out.println("-----------------------------");
+        }
+	
+        
+        int idSeguroBuscar = 1; 
+        Seguro seguroEncontrado = seguroDAO.obtenerUnSeguro(idSeguroBuscar);
+        if (seguroEncontrado != null) {
+            System.out.println("Seguro encontrado:");
+            System.out.println("ID: " + seguroEncontrado.getIdSeguro());
+            System.out.println("Descripción: " + seguroEncontrado.getDescripcion());
+            System.out.println("Tipo de Seguro: " + seguroEncontrado.getIdTipo());
+            System.out.println("Costo de Contratación: " + seguroEncontrado.getCostoContratacion());
+            System.out.println("Costo Asegurado: " + seguroEncontrado.getCostoAsegurado());
+        } else {
+            System.out.println("Seguro con ID " + idSeguroBuscar + " no encontrado.");
+        }
+
+	}
 		
 	
 }
 
-}
